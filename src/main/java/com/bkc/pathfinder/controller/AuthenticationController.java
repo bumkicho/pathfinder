a17db9dd03cfc12fcaa42b5e1ebbdd134dffe004
bumkicho <bumkicho@gmail.com>
2021-08-25 12:14:07 -0400

    verify/token should be get

--- src/main/java/com/bkc/pathfinder/controller/AuthenticationController.java
+++ src/main/java/com/bkc/pathfinder/controller/AuthenticationController.java
@@ -3,6 +3,7 @@ package com.bkc.pathfinder.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -44,7 +45,7 @@ public class AuthenticationController {
 		return new ResponseEntity<>(iUserService.registerUser(user), HttpStatus.CREATED);
 	}
 	
-	@PostMapping("verify/{token}")
+	@GetMapping("verify/{token}")
 	public ResponseEntity<String> verify(@PathVariable String token) {
 		iAuthenticationService.verify(token);
 		
